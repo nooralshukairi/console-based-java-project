b9318d13a413dbf2a75207a62d1723feea09204c
Noor Al Shukairi <noor.alshukairi@gmail.com>
2021-08-29 17:53:22 +0400

    Initial Commit

---
+++ src/consolebasedjavaproject/ConsoleBasedJavaProject.java
@@ -0,0 +1,14 @@
+
+package consolebasedjavaproject;
+
+/**
+ *
+ * @author Noor Al Shukairi
+ */
+public class ConsoleBasedJavaProject {
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
